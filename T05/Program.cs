32bceb3e013e1ddd9ea0d76a2ca0ced7367b897e
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-16 16:54:28 +0530

    Working code T05 v.01

---
+++ T05/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program {
+   static void Main () {
+      Console.WriteLine ("Enter the number to find prime number or not");
+      int i = Convert.ToInt32 (Console.ReadLine ());
+      if (i % 2 == 0) Console.WriteLine ($"{i} is not a prime number");
+      else Console.WriteLine ($"{i} is a prime number");
+   }
+}
