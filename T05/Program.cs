a9c3797d064c112bf5215ec2ea505632c5e2e9c1
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-17 14:46:51 +0530

    working code T05 v3.0

--- T05/Program.cs
+++ T05/Program.cs
@@ -1,9 +1,17 @@
-﻿// GIVEN NUMBER IS PRIME NUMBER (OR) NOT A PRIME NUMBER
+﻿// GIVEN NUMBER IS PRIME NUMBER (OR) NOT A PRIME NUMBER USING SQUARE ROOT N METHOD
 internal class Program {
    static void Main () {
-      Console.WriteLine ("Enter the number to find prime number or not");
-      int i = Convert.ToInt32 (Console.ReadLine ());
-      if (i % 2 == 0) Console.WriteLine ($"{i} is not a prime number");
-      else Console.WriteLine ($"{i} is a prime number");
+      Console.WriteLine ("Enter the number to check it's a prime number (or) not");
+      double n = Convert.ToInt32 (Console.ReadLine ()), m = Math.Sqrt (n);
+      int h = Convert.ToInt32 (Math.Round (m)); int i;
+      for (i = 2; i < h; i++) {
+         //Console.WriteLine (i);
+         if (h % i == 0) {
+            Console.WriteLine ($"\n{n} is not a prime number");
+            break;
+         }
+      }
+      //Console.WriteLine (i);
+      if (h == i) Console.WriteLine ($"\n{n} is a prime number");
    }
 }
