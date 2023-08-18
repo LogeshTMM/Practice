dfd3156aa271593e73b1badefd350a008afda9b4
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-18 15:11:29 +0530

    working code T06 v1.0

---
+++ T06/Program.cs
@@ -0,0 +1,17 @@
+﻿// PALINDROME CHECKER
+internal class Program {
+   static void Main () {
+      Console.WriteLine ("Enter the word to check palindrome (or) not");
+      string a = Console.ReadLine ().ToLower (); char b; char[] c = Array.Empty<char> ();
+      int i = a.Length - 1;
+      while (i >= 0) {
+         b = a[i];
+         c = c.Append (b).ToArray ();
+         i--;
+      }
+      var e = new string (c);
+      Console.WriteLine ($"\nReverse order of {a} is {e}");
+      if (string.Equals (a, e)) Console.WriteLine ("\nIs a palindrome");
+      else Console.WriteLine ("\nIs not a palindrome");
+   }
+}
