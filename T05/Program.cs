05f751d6aefa4cdad3bc8b48dd6a7ab27f40401e
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-18 14:58:03 +0530

    working code T05 v4.0

--- T05/Program.cs
+++ T05/Program.cs
@@ -2,16 +2,13 @@
 internal class Program {
    static void Main () {
       Console.WriteLine ("Enter the number to check it's a prime number (or) not");
-      double n = Convert.ToInt32 (Console.ReadLine ()), m = Math.Sqrt (n);
-      int h = Convert.ToInt32 (Math.Round (m)); int i;
+      int h = Convert.ToInt32 (Console.ReadLine ()); int i;
       for (i = 2; i < h; i++) {
-         //Console.WriteLine (i);
          if (h % i == 0) {
-            Console.WriteLine ($"\n{n} is not a prime number");
+            Console.WriteLine ($"\n{h} is not a prime number");
             break;
          }
       }
-      //Console.WriteLine (i);
-      if (h == i) Console.WriteLine ($"\n{n} is a prime number");
+      if (h == i) Console.WriteLine ($"\n{h} is a prime number");
    }
 }
