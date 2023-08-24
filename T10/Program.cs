1c2a789a3b9470f5e13ceca7ded8a5fdffc4fa64
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-24 16:44:31 +0530

    Working Code V1.0

---
+++ T10/Program.cs
@@ -0,0 +1,14 @@
+﻿// REVERSE A GIVEN NUMBER
+internal class Program {
+   static void Main () {
+      Console.WriteLine ("Enter the number to get reverse");
+      string n = Console.ReadLine ();
+      char[] k = { }; char l; 
+      for (int i = 1; i <= n.Length; i++) {
+         l = n[i - 1];
+         k = k.Append (l).ToArray ();
+      }
+      Array.Reverse(k);
+      Console.WriteLine (k);
+   }
+}
