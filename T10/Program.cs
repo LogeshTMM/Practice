d448c3319be77fbc93fb40fce1676b9886914d46
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-31 10:42:05 +0530

    T10: Implemented

---
+++ T10/Program.cs
@@ -0,0 +1,10 @@
+﻿// REVERSE A GIVEN NUMBER
+internal class Program {
+   static void Main () {
+      Console.WriteLine ("Enter the number to get reverse");
+      string n = Console.ReadLine ();
+      var chararray = n.ToCharArray ();
+      Array.Reverse (chararray);
+      Console.WriteLine (chararray);
+   }
+}
