966e7e54f13f3e245aeb86786544521422ff4c93
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-31 11:44:38 +0530

    T03: ^ implemented

--- T03/Program.cs
+++ T03/Program.cs
@@ -6,7 +6,7 @@ internal class Program {
       Console.WriteLine ("Enter the nth term of s fibonnaci series, to find the value and along with the series");
       int n = Convert.ToInt32 (Console.ReadLine ());
       for (int i = 0; i < n; i++) {
-         lk = fb[fb.Length - 1] + fb[fb.Length - 2];
+         lk = fb[^1] + fb[^2];
          fb = fb.Append (lk).ToArray (); if (i == n - 3) break;
       }
       Console.WriteLine ($"\nFibonnaci series of {n}th term is");
