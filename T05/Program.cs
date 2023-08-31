ef7cc0ccc3e50c105307253822fc965234e5e135
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-31 10:14:21 +0530

    T05: Implemented

---
+++ T05/Program.cs
@@ -0,0 +1,14 @@
+﻿// GIVEN NUMBER IS PRIME NUMBER (OR) NOT A PRIME NUMBER
+internal class Program {
+   static void Main () {
+      Console.WriteLine ("Enter the number to check it's a prime number (or) not");
+      int h = Convert.ToInt32 (Console.ReadLine ()); int i;
+      for (i = 2; i < h; i++) {
+         if (h % i == 0) {
+            Console.WriteLine ($"\n{h} is not a prime number");
+            break;
+         }
+      }
+      if (h == i) Console.WriteLine ($"\n{h} is a prime number");
+   }
+}
