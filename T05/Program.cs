dab849ebb5dec3752437f163bf7fa94e7ea9bae7
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-04 14:21:55 +0530

    correction implemented

--- T05/Program.cs
+++ T05/Program.cs
@@ -2,7 +2,9 @@
 internal class Program {
    static void Main () {
       Console.WriteLine ("Enter the number to check it's a prime number (or) not");
-      int h = Convert.ToInt32 (Console.ReadLine ()); int i;
+      int.TryParse (Console.ReadLine (), out int h); int i;
+      if (h == 0) Console.WriteLine ("\nInvaild Input");
+      if (h == 1) Console.WriteLine ($"{h} is not a prime number, because a prime number must divisble by itself number and also 1");
       for (i = 2; i < h; i++) {
          if (h % i == 0) {
             Console.WriteLine ($"\n{h} is not a prime number");
