1b329211ffbc0984b5264eb88261e090387df014
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-04 15:25:04 +0530

    correction implemented

--- T06/Program.cs
+++ T06/Program.cs
@@ -2,16 +2,10 @@
 internal class Program {
    static void Main () {
       Console.WriteLine ("Enter the word to check palindrome (or) not");
-      string a = Console.ReadLine ().ToLower (); char b; char[] c = Array.Empty<char> ();
-      int i = a.Length - 1;
-      while (i >= 0) {
-         b = a[i];
-         c = c.Append (b).ToArray ();
-         i--;
-      }
-      var e = new string (c);
-      Console.WriteLine ($"\nReverse order of {a} is {e}");
-      if (string.Equals (a, e)) Console.WriteLine ("\nIs a palindrome");
+      string a = Console.ReadLine (); var b = a.Reverse ().ToArray ();
+      var c = new string (b);/*Console.Write(b.GetType ());*/
+      Console.WriteLine ($"\nReverse order of {a} is {c}");
+      if (string.Equals (a, c)) Console.WriteLine ("\nIs a palindrome");
       else Console.WriteLine ("\nIs not a palindrome");
    }
 }
