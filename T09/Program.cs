a8c4f8c414ddc154c8aae2386b0844e6fc41e2e8
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-07 13:12:04 +0530

    Final version

--- T09/Program.cs
+++ T09/Program.cs
@@ -2,32 +2,35 @@
 internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter the number of rows for a diamond do you want to see");
-      int a = Convert.ToInt32 (Console.ReadLine ());
-      char b = '*'; char c = ' '; int[] s = { };
-      for (int j = 1; j <= a; j++) {
-         if (j % 2 != 0) {
-            s = s.Append (j).ToArray ();
+      int.TryParse (Console.ReadLine (), out int a);
+      if (a == 0) Console.WriteLine ("Invaild Input");
+      else {
+         char b = '*'; char c = ' '; int[] s = { };
+         for (int j = 1; j <= a; j++) { // separate a hard number from a given input
+            if (j % 2 != 0) {
+               s = s.Append (j).ToArray ();
+            }
          }
-      }
-      //Console.WriteLine (s.Length);
-      int len = s.Length - 1;
-      foreach (int j in s) {
-         var str1 = new string (c, len);
-         Console.Write (str1); len--;
-         var str = new string (b, j);
-         Console.WriteLine (str);
-      }
-      Array.Reverse (s);
-      List<int> numberList = s.ToList ();
-      numberList.RemoveAt (0);
-      //foreach (int j in s) Console.Write (j);
-      int len1 = 1;
-      foreach (int j in numberList) {
-         if (j != a) {
-            var str1 = new string (c, len1);
-            Console.Write (str1); len1++;
+         //Console.WriteLine (s.Length);
+         int len = s.Length - 1;
+         foreach (int j in s) {
+            var str1 = new string (c, len); // string (c, len) len denotes how many times repeat c
+            Console.Write (str1); len--;
             var str = new string (b, j);
             Console.WriteLine (str);
+         }
+         Array.Reverse (s);
+         List<int> numberList = s.ToList ();
+         numberList.RemoveAt (0);
+         //foreach (int j in s) Console.Write (j);
+         int len1 = 1;
+         foreach (int j in numberList) {
+            if (j != a) {
+               var str1 = new string (c, len1);
+               Console.Write (str1); len1++;
+               var str = new string (b, j);
+               Console.WriteLine (str);
+            }
          }
       }
    }
