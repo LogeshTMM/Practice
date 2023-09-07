28c78e8ee4afd9963b3834fcf6f00cdbdb7ee3c9
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-07 11:37:12 +0530

    Final version

--- T05/Program.cs
+++ T05/Program.cs
@@ -4,13 +4,13 @@ internal class Program {
       Console.WriteLine ("Enter the number to check it's a prime number (or) not");
       int.TryParse (Console.ReadLine (), out int h); int i;
       if (h == 0) Console.WriteLine ("\nInvaild Input");
-      if (h == 1) Console.WriteLine ($"{h} is not a prime number, because a prime number must divisble by itself number and also 1");
+      if (h == 1) Console.WriteLine ("1 is not a prime number, because a prime number must divisble by itself number and also divisble by 1");
       for (i = 2; i < h; i++) {
          if (h % i == 0) {
-            Console.WriteLine ($"\n{h} is not a prime number");
+            Console.WriteLine ($"{h} is not a prime number");
             break;
          }
       }
-      if (h == i) Console.WriteLine ($"\n{h} is a prime number");
+      if (h == i) Console.WriteLine ($"{h} is a prime number");
    }
 }
