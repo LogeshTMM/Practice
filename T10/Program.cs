4619e7be6380730ddd32e8c967e565eba42179fb
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-07 12:23:14 +0530

    Final version

--- T10/Program.cs
+++ T10/Program.cs
@@ -1,10 +1,13 @@
 ﻿// REVERSE A GIVEN NUMBER
 internal class Program {
    static void Main () {
-      Console.WriteLine ("Enter the number to get reverse");
-      string n = Console.ReadLine ();
-      var chararray = n.ToCharArray ();
-      Array.Reverse (chararray);
-      Console.WriteLine (chararray);
+      Console.WriteLine ("Enter the number to get the reverse order");
+      int.TryParse (Console.ReadLine (), out int n);
+      if (n == 0) Console.WriteLine ("Invaild Input");
+      else {
+         var org_n = n.ToString ();
+         Console.Write ("Reverse order of given number is" + " ");
+         for (int i = org_n.Length; i != 0; i--) Console.Write (org_n[i - 1]);
+      }
    }
 }
