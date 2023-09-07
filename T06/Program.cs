5e04625d2f3a4ecf1d705bfeeafc9f188d569342
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-07 11:42:11 +0530

    Final version

--- T06/Program.cs
+++ T06/Program.cs
@@ -3,9 +3,9 @@ internal class Program {
    static void Main () {
       Console.WriteLine ("Enter the word to check palindrome (or) not");
       string a = Console.ReadLine (); var b = a.Reverse ().ToArray ();
-      var c = new string (b);/*Console.Write(b.GetType ());*/
-      Console.WriteLine ($"\nReverse order of {a} is {c}");
-      if (string.Equals (a, c)) Console.WriteLine ("\nIs a palindrome");
-      else Console.WriteLine ("\nIs not a palindrome");
+      var c = new string (b);       /*Console.Write(b.GetType ());*/
+      Console.WriteLine ($"Reverse order of {a} is {c}");
+      if (string.Equals (a, c)) Console.WriteLine ("Is a palindrome");
+      else Console.WriteLine ("Is not a palindrome");
    }
 }
