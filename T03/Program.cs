2ad20f2c0a36bb5a71a03aa9a7285fe314d89774
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-08 13:49:35 +0530

    correction completed on Final version

--- T03/Program.cs
+++ T03/Program.cs
@@ -5,14 +5,11 @@ internal class Program {
       Console.WriteLine ("Enter the nth term of a Fibonnaci series, to find the value and along with the series");
       int.TryParse (Console.ReadLine (), out int n);
       if (n == 0) Console.WriteLine ("Invaild Input");
-      else {
-         for (int i = 0; i < n; i++) {
-            int dummy = fb_list[^1] + fb_list[^2];
-            fb_list.Add (dummy);
-         }
-         Console.WriteLine ($"Fibonnaci series is ");
-         foreach (int j in fb_list) Console.Write (j + " "); // (or) foreach (int j in fb_list) Console.Write ($"{j} ");
-         Console.WriteLine ($"\n{n}th term value of fibonnaci series is {fb_list[n]}");
+      else if (n != 0) {
+         for (int i = 0; i < n; i++) fb_list.Add (fb_list[^1] + fb_list[^2]);
+         Console.Write ($"Fibonnaci series is ");
+         for (int i = 0; i < n; i++) Console.Write (fb_list[i] + " ");
+         Console.WriteLine ($"\nThe value of {n}th term in fibonnaci series is {fb_list[n - 1]}");
       }
    }
 }
