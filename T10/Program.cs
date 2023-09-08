2c5dd11ecca119fad356c1957129791730670b0d
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-08 16:39:53 +0530

    Correction Implemented

--- T10/Program.cs
+++ T10/Program.cs
@@ -2,12 +2,19 @@
 internal class Program {
    static void Main () {
       Console.WriteLine ("Enter the number to get the reverse order");
-      int.TryParse (Console.ReadLine (), out int n);
+      long.TryParse (Console.ReadLine (), out long n);
       if (n == 0) Console.WriteLine ("Invaild Input");
-      else {
-         var org_n = n.ToString ();
+      var org_n = n.ToString (); var dup = Array.Empty<char> (); char dummy;
+      if (n != 0) {
          Console.Write ("Reverse order of given number is" + " ");
-         for (int i = org_n.Length; i != 0; i--) Console.Write (org_n[i - 1]);
+         for (int i = org_n.Length; i != 0; i--) {
+            Console.Write (org_n[i - 1]);
+            dummy = org_n[i - 1];
+            dup = dup.Append (dummy).ToArray ();
+         }
+         string dup_n = new string (dup);
+         if (string.Equals (org_n, dup_n)) Console.Write ("\nIt's a palindrome");
+         else Console.Write ("\nIt's not a palindrome");
       }
    }
 }
