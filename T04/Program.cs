5e804448ada9eed2914205cae7b177c8efc09daf
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-11 16:06:27 +0530

    T04 Final version

--- T04/Program.cs
+++ T04/Program.cs
@@ -2,11 +2,11 @@
 internal class Program {
    static void Main () {
       Console.WriteLine ("Enter the value of a and b, to find LCM and GCD between them");
-      int.TryParse (Console.ReadLine (), out int a);
-      int.TryParse (Console.ReadLine (), out int b);
+      uint.TryParse (Console.ReadLine (), out uint a); uint.TryParse (Console.ReadLine (), out uint b);
+      //Math.Abs (a); Math.Abs (b);
       if (a == 0 || b == 0) Console.WriteLine ("\nInvaild Input");
       else {
-         int m = Math.Max (a, b);
+         uint m = Math.Max (a, b);
          while (m != 0) {
             if (m % a == 0 && m % b == 0) {
                Console.WriteLine ($"LCM of a given number {a} and {b} is {m}");
@@ -15,7 +15,7 @@ internal class Program {
             m++;
          }
          if (m != 0) {
-            int p = (a * b) / m;
+            uint p = (a * b) / m;
             Console.WriteLine ($"GCD of a given number {a} and {b} is {p}");
          }
       }
