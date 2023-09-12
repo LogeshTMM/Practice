946aa69536c8807353cf4ef9c2a7e6b90c44f6f8
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-12 13:53:47 +0530

    corrected

--- T16/Program.cs
+++ T16/Program.cs
@@ -2,25 +2,18 @@
 internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter the word to check ABECEDARIAN WORD or not");
-      string a = Console.ReadLine (); a.ToLower (); var b = a.ToCharArray (); var c = new List<int> ();
-      int d; var c_dup = new List<int> ();
-      //Console.WriteLine (b[0] == 97);
-      //Console.WriteLine (a.Length);
-      if (a.All (char.IsLetter)) {
-         for (int i = 0; i < a.Length; i++) {
-            d = b[i] - 96;
-            c.Add (d);
+      string input = Console.ReadLine ().ToLower ();
+      var chars = input.ToCharArray ();
+      var charints = new List<int> ();
+      if (input.All (char.IsLetter)) {
+         for (int i = 0; i < input.Length; i++) {
+            int d = chars[i] - 96;
+            charints.Add (d);
          }
-         //c.CopyTo(c_dup); 
-         //c.GetRange (0, c.Count);
-         c_dup = c.GetRange (0, c.Count);
-         Console.WriteLine ("Length of a given word is " + c.Count);
-         c_dup.Sort ();
-         //foreach (var e in c) Console.Write (e + " "); Console.WriteLine ();
-         //foreach (var e in c_dup) Console.Write (e + " ");
-         bool isequal = Enumerable.SequenceEqual (c, c_dup);
-         if (isequal) Console.WriteLine ($"{a} is an ABECEDARIAN WORD");
-         else Console.WriteLine ($"{a} is not an ABECEDARIAN WORD");
+         List<int> sortedarray = charints.GetRange (0, charints.Count);
+         sortedarray.Sort ();
+         bool isequal = Enumerable.SequenceEqual (charints, sortedarray);
+         Console.WriteLine ($"{input} {(isequal ? "is" : "not")} an ABECEDARIAN WORD");
       } else Console.WriteLine ("Invalid Input");
    }
 }
