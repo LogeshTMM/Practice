e3bed740a349cdc6270722e2d2a4c57b5f21c2d9
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-13 17:01:00 +0530

    T13 implemented:

--- T13/Program.cs
+++ T13/Program.cs
@@ -2,23 +2,43 @@
 internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter the Password to check whether it's strong (or) weak");
-      string pw = Console.ReadLine ();
-      List<bool> special_char = new ();
-      foreach (char c in pw) special_char.Add (char.IsLetterOrDigit (c));
-      bool d = pw.Any (char.IsDigit);
-      bool u = pw.Any (char.IsUpper);
-      bool l = pw.Any (char.IsLower);
-      bool s = special_char.Contains (false);
+      string pw = Console.ReadLine (), special = "!@#$%^&*()_+/";
+      bool asDigit, asUpper, asLower, hasSpecial;
+      asDigit = asUpper = asLower = hasSpecial = false;
+      for (int i = 0; i < pw.Length; i++) {
+         if (pw.Any (char.IsDigit)) {
+            asDigit = true;
+            break;
+         }
+      }
+      for (int i = 0; i < pw.Length; i++) {
+         if (pw.Any (char.IsUpper)) {
+            asUpper = true;
+            break;
+         }
+      }
+      for (int i = 0; i < pw.Length; i++) {
+         if (pw.Any (char.IsLower)) {
+            asLower = true;
+            break;
+         }
+      }
+      for (int i = 0; i < pw.Length; i++) {
+         if (special.Contains (pw[i])) {
+            hasSpecial = true;
+            break;
+         }
+      }
       bool pw_len = pw.Length > 6;
-      if (d && u && l && s && pw_len) Console.WriteLine ("Strong Password, you have.");
+      if (asDigit && asUpper && asLower && hasSpecial && pw_len) Console.WriteLine ("Strong Password, you have.");
       else {
-         Console.Write ("\nYou have a weak Password, reason is given below\n");
-         if (!pw_len) Console.WriteLine ("* Length of a password should be more than 6");
-         if (!d) Console.WriteLine ("* It contains at least one digit.");
-         if (!u) Console.WriteLine ("* It contains at least one upper character.");
-         if (!l) Console.WriteLine ("* It contains at least one lower character.");
-         if (!s) Console.WriteLine ("* It contains at least one special character.");
-         Console.WriteLine ("Try again for a strong password");
+         Console.Write ("You have a weak Password, reason is given below:\r\n");
+         if (!pw_len) Console.WriteLine ("   " + "* Length of a password should be more than 6");
+         if (!asDigit) Console.WriteLine ("   " + "* It contains at least one digit.");
+         if (!asUpper) Console.WriteLine ("   " + "* It contains at least one upper character.");
+         if (!asLower) Console.WriteLine ("   " + "* It contains at least one lower character.");
+         if (!hasSpecial) Console.WriteLine ("   " + "* It contains at least one special character.");
+         Console.WriteLine ("Try again for a strong password.");
       }
    }
 }
