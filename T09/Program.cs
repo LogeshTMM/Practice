ae82883f78c4d6b21d422e0f59ee912c81b659f3
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-13 10:57:20 +0530

    correction done

--- T09/Program.cs
+++ T09/Program.cs
@@ -2,35 +2,35 @@
 internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter the number of rows for a diamond do you want to see");
-      if (uint.TryParse (Console.ReadLine (), out uint a) && a >= 5) {
-         char b = '*'; char c = ' '; int[] s = Array.Empty<int> ();
-         for (int j = 1; j <= a; j++) { // separate a hard number from a given input
+      int.TryParse (Console.ReadLine (), out int input);
+      if (input == 0 || input < 5) {
+         Console.WriteLine ("Invaild Input");
+         return;
+      } else {
+         char star = '*'; char space = ' '; int[] noofstars = Array.Empty<int> ();
+         for (int j = 1; j <= input; j++) {
             if (j % 2 != 0) {
-               s = s.Append (j).ToArray ();
+               noofstars = noofstars.Append (j).ToArray ();
             }
          }
-         //Console.WriteLine (s.Length);
-         int len = s.Length - 1;
-         foreach (int j in s) {
-            var str1 = new string (c, len); // string (c, len) len denotes how many times repeat c
-            Console.Write (str1); len--;
-            var str = new string (b, j);
-            Console.WriteLine (str);
+         int repeat = noofstars.Length - 1;
+         foreach (int j in noofstars) {
+            var space_str = new string (space, repeat);
+            Console.Write (space_str); repeat--;
+            var star_str = new string (star, j);
+            Console.WriteLine (star_str);
          }
-         Array.Reverse (s);
-         s = s.Skip (1).ToArray ();
-         //List<int> numberList = s.ToList ();
-         //numberList.RemoveAt (0);
-         int len1 = 1;
-         foreach (int j in s) {
-            if (j != a) {
-               var str1 = new string (c, len1);
-               Console.Write (str1); len1++;
-               var str = new string (b, j);
-               Console.WriteLine (str);
+         Array.Reverse (noofstars); int len = 1;
+         for (int i = 1; i < noofstars.Length; i++) {
+            var item = noofstars[i];
+            if (i != 0) {
+               var space_str = new string (space, len);
+               Console.Write (space_str);
+               var star_str = new string (star, item);
+               Console.WriteLine (star_str);
+               len++;
             }
          }
-         //foreach (int j in s) Console.Write (j);
-      } else Console.WriteLine ("Invaild Input");
+      }
    }
 }
