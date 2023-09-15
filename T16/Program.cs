7fe830c682888c75e67bf361b3cc4fdd317bbede
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-15 14:51:54 +0530

    T16 implemented

--- T16/Program.cs
+++ T16/Program.cs
@@ -4,16 +4,12 @@ internal class Program {
       Console.WriteLine ("Enter the word to check ABECEDARIAN WORD or not");
       string input = Console.ReadLine ().ToLower ();
       var chars = input.ToCharArray ();
-      var charints = new List<int> ();
-      if (input.All (char.IsLetter)) {
-         for (int i = 0; i < input.Length; i++) {
-            int d = chars[i] - 96;
-            charints.Add (d);
-         }
-         List<int> sortedarray = charints.GetRange (0, charints.Count);
-         sortedarray.Sort ();
-         bool isequal = Enumerable.SequenceEqual (charints, sortedarray);
+      if (!input.All (char.IsLetter)) {
+         Console.WriteLine ("Invalid input");
+      } else {
+         var sortedchars = chars.Order ();
+         bool isequal = Enumerable.SequenceEqual (chars, sortedchars);
          Console.WriteLine ($"{input} {(isequal ? "is" : "not")} an ABECEDARIAN WORD");
-      } else Console.WriteLine ("Invalid Input");
+      }
    }
 }
