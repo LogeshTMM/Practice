78234516dd3be787680bad85383fe14be992f2e4
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-20 16:34:21 +0530

    T09

--- T09/Program.cs
+++ T09/Program.cs
@@ -4,32 +4,28 @@ internal class Program {
       Console.WriteLine ("Enter the number of rows for a diamond do you want to see");
       int.TryParse (Console.ReadLine (), out int input);
       if (input == 0 || input < 5) {
-         Console.WriteLine ("Invaild Input");
+         Console.WriteLine ("Invaild input");
          return;
-      } else {
-         char star = '*'; char space = ' '; int[] noofstars = Array.Empty<int> ();
-         for (int j = 1; j <= input; j++) {
-            if (j % 2 != 0) {
-               noofstars = noofstars.Append (j).ToArray ();
-            }
-         }
-         int repeat = noofstars.Length - 1;
-         foreach (int j in noofstars) {
-            var space_str = new string (space, repeat);
-            Console.Write (space_str); repeat--;
-            var star_str = new string (star, j);
+      }
+      char star = '*'; char space = ' '; List<int> numOfStars = new ();
+      for (int j = 1; j <= input; j++)
+         if (j % 2 != 0) numOfStars.Add (j);
+      int repeat = numOfStars.Count - 1;
+      foreach (int j in numOfStars) {
+         var space_str = new string (space, repeat);
+         Console.Write (space_str); repeat--;
+         var star_str = new string (star, j);
+         Console.WriteLine (star_str);
+      }
+      numOfStars.Reverse (); int len = 1;
+      for (int i = 1; i < numOfStars.Count; i++) {
+         var item = numOfStars[i];
+         if (i != 0) {
+            var space_str = new string (space, len);
+            Console.Write (space_str);
+            var star_str = new string (star, item);
             Console.WriteLine (star_str);
-         }
-         Array.Reverse (noofstars); int len = 1;
-         for (int i = 1; i < noofstars.Length; i++) {
-            var item = noofstars[i];
-            if (i != 0) {
-               var space_str = new string (space, len);
-               Console.Write (space_str);
-               var star_str = new string (star, item);
-               Console.WriteLine (star_str);
-               len++;
-            }
+            len++;
          }
       }
    }
