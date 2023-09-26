429fa9f788de0bdf52149eb640ccd4dfac80aca6
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-26 14:42:14 +0530

    T18 implemented

---
+++ T18/Program.cs
@@ -0,0 +1,27 @@
+﻿// WHETHER IS AN ARMSTRONG NUMBER OR NOT
+internal class Program {
+   static void Main (string[] args) {
+      Console.WriteLine ("Enter the number to find whether is an Armstrong Number (or) Not");
+      string input = Console.ReadLine ();
+      if (!input.All (char.IsDigit)) {
+         Console.WriteLine ("Invalid input");
+         return;
+      }
+      double sum = 0;
+      int result = 0, inputInts, count = 0;
+      int ArmStrong (int b) {
+         sum = Math.Pow (b, input.Length);
+         result += (int)sum;
+         return result;
+      }
+      foreach (int a in input) {
+         inputInts = a - 48;
+         ArmStrong (inputInts);
+         count++;
+      }
+      if (count == input.Length) {
+         if (Convert.ToInt32 (input) == result) Console.WriteLine ("Given number is an Armstrong Number");
+         else Console.WriteLine ("Given number is not an Armstrong Number");
+      }
+   }
+}
