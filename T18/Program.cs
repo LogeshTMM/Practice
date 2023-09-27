31b5174e5530ef6ff121b413fc4a7a5e4d76b412
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-27 15:02:54 +0530

    T18 implemented

--- T18/Program.cs
+++ T18/Program.cs
@@ -7,15 +7,12 @@ internal class Program {
          Console.WriteLine ("Invalid input");
          return;
       }
-      int result = 0, inputInts;
-      foreach (int a in input) {
-         inputInts = a - 48; // ASCII CODE FOR 1 is 49, here I subtract 'a' by 48 (inputInts = a-48;) to get int value 1. According to 'char 1'. 
-         ArmStrong (inputInts);
-      }
+      int result = 0;
+      foreach (int a in input) ArmStrong (a - 48); /* ASCII CODE FOR 1 is 49, assume that a subtract by 48 (a - 48;) 
+                                                     to get int value 1. According to 'char 1'.*/
       if (int.Parse (input) == result) Console.WriteLine ("Given number is an Armstrong number");
       else Console.WriteLine ("Given number is not an Armstrong number");
 
-      //Local function Armstrong()
       int ArmStrong (int b) {
          result += (int)(Math.Pow (b, input.Length));
          return result;
