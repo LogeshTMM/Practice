d57d8183d823a775eb0f4cd837eacababf04fdb3
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-27 09:20:19 +0530

    T05 implemented

--- T05/Program.cs
+++ T05/Program.cs
@@ -2,14 +2,12 @@
 internal class Program {
    static void Main () {
       Console.WriteLine ("Enter the number to check it's a prime number (or) not");
-      int i;
       if (int.TryParse (Console.ReadLine (), out int num) && num > 0) {
          if (num == 1) {
             Console.WriteLine ("1 is not a prime number, because a prime number" + " " +
                                "must divisble by itself number and also divisble by 1");
-            return;
          } else {
-            for (i = 2; i < num; i++) {
+            for (int i = 2; i < num; i++) {
                if (num % i == 0) {
                   Console.WriteLine ($"{num} is not a prime number");
                   return;
