7c1294981b556ca4afe67331abfdb77ccb0bc4dd
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-27 14:49:19 +0530

    T18.1 implemented

--- T18.1/Program.cs
+++ T18.1/Program.cs
@@ -1,37 +1,27 @@
 ﻿// TO FIND NTH TERM ARMSTRONG NUMBER
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Enter the nth number to find the Armstrong Number");
+      Console.WriteLine ("Enter the nth number to find the Armstrong number");
       int.TryParse (Console.ReadLine (), out int num);
       if (num == 0) {
          Console.WriteLine ("Invalid input");
          return;
       }
-      int result = 0, a, count = 0;
-      List<int> lists = new (), ntermlist = new ();
-      string dString;
-      double sum;
-      int ArmStrong (int tempNum) {
-         for (int i = 0; i >= 0; i++) {
-            dString = i.ToString ();
-            foreach (char b in dString) {
-               a = b - 48;
-               sum = Math.Pow (a, dString.Length);
-               lists.Add ((int)sum);
-            }
-            result = lists.Sum ();
-            if (i == result) {
-               ntermlist.Add (i);
-               count++;
-               if (count == num) {
-                  Console.WriteLine ($"{num} th term of Armstrong Number is {i}");
-                  break;
-               }
-            }
-            lists.Clear ();
-         }
-         return result;
-      }
       ArmStrong (num);
+   }
+   static int ArmStrong (int nthTerm) {
+      int count = 0;
+      for (int i = 0; i >= 0; i++) {
+         int result = 0;
+         foreach (int b in i.ToString ()) result += (int)Math.Pow (b - 48, i.ToString ().Length);
+         if (i == result) {
+            count++;
+            if (count == nthTerm) {
+               Console.WriteLine ($"{nthTerm} th term of Armstrong number is {i}");
+               break;
+            }
+         }
+      }
+      return count;
    }
 }
