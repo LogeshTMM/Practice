a6f21aa8134f44c8d95864f2105d45dd97df209b
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-27 15:50:51 +0530

    T18.1 implemented

--- T18.1/Program.cs
+++ T18.1/Program.cs
@@ -2,26 +2,28 @@
 internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter the nth number to find the Armstrong number");
-      int.TryParse (Console.ReadLine (), out int num);
-      if (num == 0) {
+      int.TryParse (Console.ReadLine (), out int input);
+      if (input == 0) {
          Console.WriteLine ("Invalid input");
          return;
       }
-      ArmStrong (num);
+      ArmStrong (input);
    }
    static int ArmStrong (int nthTerm) {
-      int count = 0;
-      for (int i = 0; i >= 0; i++) {
+      int count = 0, num = 0;
+      while (num >= 0) {
          int result = 0;
-         foreach (int b in i.ToString ()) result += (int)Math.Pow (b - 48, i.ToString ().Length);
-         if (i == result) {
+         foreach (int a in num.ToString ()) result += (int)Math.Pow (a - 48, num.ToString ().Length);
+         /* ASCII CODE FOR 1 is 49, assume that subtract a by 48 (a - 48;) to get int value 1.*/
+         if (num == result) {
             count++;
             if (count == nthTerm) {
-               Console.WriteLine ($"{nthTerm} th term of Armstrong number is {i}");
+               Console.WriteLine ($"{nthTerm} th term of Armstrong number is {num}");
                break;
             }
          }
+         num++;
       }
-      return count;
+      return num;
    }
 }
