a8d3a553b0ebe22d0250791ae8aa090ef8197e19
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-28 15:05:57 +0530

    T14 implemented

--- T14/Program.cs
+++ T14/Program.cs
@@ -6,35 +6,38 @@
          Console.WriteLine ("Invaild input");
          return;
       }
-      int count, len = input.Length;
-      List<char> order_charlist = new (); List<int> chars_count = new ();
-      for (int i = 0; i < len; i++) {
-         count = 0;
-         for (int j = 0; j < len; j++) {
-            if (input[i] == input[j]) count++;
-         }
-         chars_count.Add (count);
-      }
+      int count, inputLen = input.Length;
+      List<char> inputCharList = new ();
       for (int i = 97; i <= 123; i++) {
-         count = 0;
-         for (int j = 0; j < len; j++) {
-            if (input[j] == i) {
+         count = 0; int k = 0;
+         for (int j = 0; j < inputLen; j++) {
+            if (i == input[j]) {
                count++;
-               if (count == 1) order_charlist.Add (input[j]);
+               k = j;
             }
          }
+         if (count % 2 != 0) inputCharList.Add (input[k]);
       }
-      foreach (var a in order_charlist) Console.Write (a);
-      Console.WriteLine ("\r\nDo you want to know, how many letters repititive in your word." + " " +
-         "Then press \"y\" (or) don't want to known, then press \"n\"");
+      if (inputCharList.Count == 0) Console.Write ("\"\""); // Display Empty string on screen
+      else foreach (var a in inputCharList) Console.Write (a);
+      Console.WriteLine ("\r\nDo you want to know, how many letters are repititive in your word " +
+         "then press \"y\". If you don't want to known, then press any button to exit.");
+      List<int> inputCharsCount = new ();
+      for (int i = 0; i < inputLen; i++) {
+         count = 0;
+         for (int j = 0; j < inputLen; j++) {
+            if (input[i] == input[j]) count++;
+         }
+         inputCharsCount.Add (count);
+      }
       char userinput = Console.ReadKey (true).KeyChar;
       if (userinput == 'y') {
          for (int i = 97; i <= 123; i++) {
             count = 0;
-            for (int j = 0; j < len; j++) {
+            for (int j = 0; j < inputLen; j++) {
                if (input[j] == i) {
                   count++;
-                  if (count == 1) Console.Write ($"{input[j]} = {chars_count[j]}\r\n");
+                  if (count == 1) Console.Write ($"{input[j]} = {inputCharsCount[j]}\r\n");
                }
             }
          }
