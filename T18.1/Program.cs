a2908e98ea9fa5f7e1bf3fcc6930eb3b67c2b92e
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-28 09:48:19 +0530

    T18.1 implementd'

--- T18.1/Program.cs
+++ T18.1/Program.cs
@@ -2,16 +2,15 @@
 internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter the nth number to find the Armstrong number");
-      int.TryParse (Console.ReadLine (), out int input);
-      if (input == 0) {
+      if (!uint.TryParse (Console.ReadLine (), out uint input)) {
          Console.WriteLine ("Invalid input");
          return;
       }
       ArmStrong (input);
    }
-   static int ArmStrong (int nthTerm) {
+   static int ArmStrong (uint nthTerm) {
       int count = 0, num = 0;
-      while (num >= 0) {
+      while (true) {
          int result = 0;
          foreach (int a in num.ToString ()) result += (int)Math.Pow (a - 48, num.ToString ().Length);
          /* ASCII CODE FOR 1 is 49, assume that subtract a by 48 (a - 48;) to get int value 1.*/
