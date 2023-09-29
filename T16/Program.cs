42305b1ba3282dfe165938b0e7bd37ca7fc22b1e
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-29 10:15:41 +0530

    T16 implemented

--- T16/Program.cs
+++ T16/Program.cs
@@ -3,28 +3,24 @@ internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter the word to check ABECEDARIAN WORD or not");
       string input = Console.ReadLine ().ToLower ();
-      List<int> chars = new (); List<bool> checkchars = new ();
+      List<int> intChars = new (); List<bool> boolIntChars = new ();
       if (!input.All (char.IsLetter)) {
          Console.WriteLine ("Invalid input");
       } else {
          for (int i = 0; i < input.Length; i++) {
             int a = input[i] - 96;
-            chars.Add (a);
+            intChars.Add (a);
          }
-         foreach (var b in chars) { Console.WriteLine (b); }
-         int count = 1; int loopcontrol = chars.Count - 1;
-         for (int j = 0; j < loopcontrol; j++, count++) {
-            if (j == count) {
-               bool last = (chars[(chars.Count) - 1]) < (chars[(chars.Count)]);
-               checkchars.Add (last);
-            } else if (count < chars.Count) {
-               if (chars[j] < chars[count]) checkchars.Add (true);
-               else checkchars.Add (false);
+         for (int i = 0, j = 1; j < intChars.Count; i++, j++) {
+            if (intChars[i] < intChars[j]) {
+               boolIntChars.Add (true);
+            } else {
+               boolIntChars.Add (false);
+               Console.WriteLine ($"{input} not an ABECEDARIAN word");
+               return;
             }
          }
-         foreach (var a in checkchars) Console.WriteLine (a);
-         if (checkchars.Contains (false)) Console.WriteLine ($"{input} not an ABECEDARIAN WORD");
-         else Console.WriteLine ($"{input} is an ABECEDARIAN WORD");
+         if (!boolIntChars.Contains (false)) Console.WriteLine ($"{input} is an ABECEDARIAN word");
       }
    }
 }
