a85c0479758991d8eb8e1345707ea81952ca94fa
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-29 13:43:18 +0530

    T16 implemented

--- T16/Program.cs
+++ T16/Program.cs
@@ -3,24 +3,21 @@ internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter the word to check it's an ABECEDARIAN word or not");
       string input = Console.ReadLine ().ToLower ();
-      List<int> intChars = new (); List<bool> boolIntChars = new ();
       if (!input.All (char.IsLetter)) {
          Console.WriteLine ("Invalid input");
-      } else {
-         for (int i = 0; i < input.Length; i++) {
-            int a = input[i] - 96;
-            intChars.Add (a);
-         }
-         for (int i = 0, j = 1; j < intChars.Count; i++, j++) {
-            if (intChars[i] < intChars[j]) {
-               boolIntChars.Add (true);
-            } else {
-               boolIntChars.Add (false);
-               Console.WriteLine ($"{input} not an ABECEDARIAN word");
-               return;
-            }
-         }
-         if (!boolIntChars.Contains (false)) Console.WriteLine ($"{input} is an ABECEDARIAN word");
+         return;
       }
+      List<int> intChars = new ();
+      for (int i = 0; i < input.Length; i++) {
+         int a = input[i] - 96;
+         intChars.Add (a);
+      }
+      for (int i = 0, j = 1; j < intChars.Count; i++, j++) {
+         if (!(intChars[i] < intChars[j])) {
+            Console.WriteLine ($"{input} not an ABECEDARIAN word");
+            return;
+         }
+      }
+      Console.WriteLine ($"{input} is an ABECEDARIAN word");
    }
 }
