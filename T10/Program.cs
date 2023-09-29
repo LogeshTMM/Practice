80f2027adcd6ded55726ea9cae07c83bbbdbf51d
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-09-29 12:06:28 +0530

    T10 implemented

--- T10/Program.cs
+++ T10/Program.cs
@@ -2,19 +2,19 @@
 internal class Program {
    static void Main () {
       Console.WriteLine ("Enter the number to get the reverse order of it");
-      if (!ulong.TryParse (Console.ReadLine (), out ulong input)) {
+      string input = Console.ReadLine ();
+      if (!input.All (char.IsDigit)) {
          Console.WriteLine ("Invaild input");
          return;
       }
-      var stringInput = input.ToString (); var reverseInputArray = Array.Empty<char> ();
-      if (input != 0) {
-         Console.Write ("Reverse order of given number is" + " ");
-         for (int i = stringInput.Length; i != 0; i--) {
-            Console.Write (stringInput[i - 1]);
-            reverseInputArray.Append (stringInput[i - 1]).ToArray ();
-         }
-         if (string.Equals (stringInput, new string (reverseInputArray))) Console.Write ("It's a palindrome");
-         else Console.Write ("\nIt's not a palindrome");
-      } else Console.WriteLine ("Invaild input");
+      List<char> reverseInputArray = new ();
+      Console.Write ("Reverse order of given number is" + " ");
+      for (int i = input.Length - 1; i >= 0; i--) {
+         Console.Write (input[i]);
+         reverseInputArray.Add (input[i]);
+      }
+      var item = new string (reverseInputArray.ToArray ());
+      if (string.Equals (input, item)) Console.Write ("\nIt's a palindrome");
+      else Console.Write ("\nIt's not a palindrome");
    }
 }
