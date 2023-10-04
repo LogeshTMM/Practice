ee5961c3a221f09f7ddac547d16ca07e9aed2252
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-10-04 12:03:53 +0530

    T19 implemented

---
+++ T19/Program.cs
@@ -0,0 +1,15 @@
+﻿// FACTORIAL OF A GIVEN NUMBER
+internal class Program {
+   static void Main () {
+      Console.WriteLine ("Enter the number to known it's factorial value");
+      if (!uint.TryParse (Console.ReadLine (), out uint input)) {
+         Console.WriteLine ("Invaild input");
+         return;
+      }
+      Console.WriteLine ($"Factorial of {input} is : {Factorial (input)}");
+   }
+   static ulong Factorial (uint n) {
+      if (n == 1) return 1;
+      return n * Factorial (n - 1);
+   }
+}
