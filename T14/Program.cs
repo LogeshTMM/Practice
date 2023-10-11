34890b3b6ef260457dd7dc77af103902548831dd
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-10-11 15:20:34 +0530

    T14 correction implemented'

--- T14/Program.cs
+++ T14/Program.cs
@@ -1,30 +1,38 @@
-﻿internal class Program {
+﻿// REDUCE REPATED STRING
+internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter the word to reduce it's repeated letters");
-      string input = Console.ReadLine ().ToLower ();
+      string input = Console.ReadLine ();
       if (!input.All (char.IsLetter)) {
          Console.WriteLine ("Invaild input");
          return;
       }
       List<char> inputCharsList = new (), tempInput = input.ToList ();
+      int lastRepIndex = 0;
       for (int i = 0; i < tempInput.Count; i++) {
          int count = 0; char item = ' ';
          for (int j = 0; j < tempInput.Count; j++) {
             if (tempInput[i] == tempInput[j]) {
-               count++;
                item = tempInput[j];
-            }
-         }
-         if (count % 2 != 0) {
-            inputCharsList.Add (item);
-            while (count != 0) {
-               tempInput.Remove (item);
+               lastRepIndex = j;
+               count++;
+            } else if (count == 1) {
+               tempInput.RemoveAt (lastRepIndex);
+               inputCharsList.Add (item);
                count--;
+               j = -1;
+            } else if (count % 2 != 0 || count % 2 == 0) {
+               while (count != 0) {
+                  tempInput.RemoveAt (lastRepIndex);
+                  lastRepIndex--;
+                  count--;
+               }
+               j = -1;
             }
-            i--;
          }
+         if (count == 0 || count == 1) inputCharsList.Add (tempInput[i]);
       }
-      if (inputCharsList.Count == 0) Console.Write ("\"\""); // Display an empty string on the screen.
+      if (inputCharsList.Count == 0) Console.Write ("\"\"");
       else foreach (var a in inputCharsList) Console.Write (a);
    }
 }
