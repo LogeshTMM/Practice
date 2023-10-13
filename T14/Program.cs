957480745d238d0c9fea6cfd8a9cf8230d083c30
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-10-13 11:57:52 +0530

    T14 correction implemented

--- T14/Program.cs
+++ T14/Program.cs
@@ -7,11 +7,17 @@ internal class Program {
          Console.WriteLine ("Invaild input");
          return;
       }
+      string result = AvoidAdjPairs (input);
+      if (result == "") Console.WriteLine ("\"\"");
+      else foreach (char a in result) Console.Write (a);
+   }
+
+   static string AvoidAdjPairs (string input) {
       List<char> inputCharsList = new (), tempInput = input.ToList ();
-      int lastRepIndex = 0;
+      int lastRepIndex = 0, j;
       for (int i = 0; i < tempInput.Count; i++) {
          int count = 0; char item = ' ';
-         for (int j = 0; j < tempInput.Count; j++) {
+         for (j = 0; j < tempInput.Count; j++) {
             if (tempInput[i] == tempInput[j]) {
                item = tempInput[j];
                lastRepIndex = j;
@@ -21,18 +27,41 @@ internal class Program {
                inputCharsList.Add (item);
                count--;
                j = -1;
-            } else if (count % 2 != 0 || count % 2 == 0) {
+            } else if (count % 2 == 0) {
                while (count != 0) {
                   tempInput.RemoveAt (lastRepIndex);
                   lastRepIndex--;
                   count--;
                }
                j = -1;
+            } else {
+               while (count != 0) {
+                  tempInput.RemoveAt (lastRepIndex);
+                  lastRepIndex--;
+                  count--;
+               }
+               inputCharsList.Add (item);
+               j = -1;
             }
          }
-         if (count == 0 || count == 1) inputCharsList.Add (tempInput[i]);
+         if (count % 2 != 0) {
+            while (count != 0) {
+               tempInput.RemoveAt (lastRepIndex);
+               lastRepIndex--;
+               count--;
+            }
+            inputCharsList.Add (item);
+            j = -1;
+         } else if (count == 0 || count == 1) inputCharsList.Add (tempInput[i]);
+         else if (count % 2 == 0) {
+            while (count != 0) {
+               tempInput.RemoveAt (lastRepIndex);
+               lastRepIndex--;
+               count--;
+            }
+         }
       }
-      if (inputCharsList.Count == 0) Console.Write ("\"\"");
-      else foreach (var a in inputCharsList) Console.Write (a);
+      if (inputCharsList.Count == 2) if (inputCharsList[0] == inputCharsList[1]) inputCharsList.Clear ();
+      return new string (inputCharsList.ToArray ());
    }
 }
