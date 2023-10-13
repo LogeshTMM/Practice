89399eb0abf3b220088afb74e7ab6158835248ec
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-10-13 09:40:12 +0530

    T21 correction implemented

--- T21/Program.cs
+++ T21/Program.cs
@@ -1,16 +1,38 @@
-﻿// SWAP A GIVEN NUMBER
+﻿// ---------------------------------------------------------------------------------------
+
+// Training ~ A training program for new joinees at Metamation, Batch- July 2023
+
+// Copyright (c) Metamation India.
+
+// ---------------------------------------------------------------------------
+
+// Program.cs
+
+// <TODO: // By getting the two integers value from the user, then swap the values.
+
+// If the output shows invalid input, then you enter other than the numbers.
+
+// Besides it shows the before and after swapping the values of a and b.>
+
+// ---------------------------------------------------------------------------------------
+
 internal class Program {
    static void Main () {
       Console.WriteLine ("Enter the vaules of a and b");
       bool firstValue = int.TryParse (Console.ReadLine (), out int a);
       bool secondValue = int.TryParse (Console.ReadLine (), out int b);
       if (!firstValue || !secondValue) Console.WriteLine ("Invaild input");
-      else SwapTwoNumbers (a, b);
+      else {
+         Console.WriteLine ($"\nBefore altering the values.\na = {a}\nb = {b}\n");
+         SwapTwoNumbers (ref a, ref b);
+         Console.WriteLine ($"After altering the values.\na = {a}\nb = {b}");
+      }
    }
-   static void SwapTwoNumbers (int b, int a) {
-      Console.WriteLine ("Do you want to swap the values of a and b, then press 'y'.");
-      char userKey = Console.ReadKey (true).KeyChar;
-      if (userKey == 'y') Console.Write ($"After swapping the values of\na = {a} \nb = {b}");
-      else Console.WriteLine ("Sorry, you press the wrong button.");
+
+   static void SwapTwoNumbers (ref int a, ref int b) {
+      var temp = a;
+      a = b;
+      b = temp;
+      return;
    }
 }
