dddc009abc4d90f033aa65027a9c9757b7c22ad8
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-10-16 13:44:52 +0530

    T21 implemented

--- T21/Program.cs
+++ T21/Program.cs
@@ -1,20 +1,14 @@
-﻿// ---------------------------------------------------------------------------------------
-
-// Training ~ A training program for new joinees at Metamation, Batch- July 2023
+﻿// Training ~ A training program for new joinees at Metamation, Batch- July 2023
 
 // Copyright (c) Metamation India.
 
-// ---------------------------------------------------------------------------
-
 // Program.cs
 
-// <TODO: // By getting the two integers value from the user, then swap the values.
+// <TODO: By getting the two integers value from the user, then swap the values.
 
 // If the output shows invalid input, then you enter other than the numbers.
 
 // Besides it shows the before and after swapping the values of a and b.>
-
-// ---------------------------------------------------------------------------------------
 
 internal class Program {
    static void Main () {
