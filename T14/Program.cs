ba8468910acd4110c958992bc1fe0398584ed133
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-10-27 09:41:07 +0530

    T14 correction implemented'

--- T14/Program.cs
+++ T14/Program.cs
@@ -8,13 +8,34 @@ internal class Program {
          return;
       }
       string result = AvoidAdjPairs (input);
-      if (result == "") Console.WriteLine ("\"\"");
-      else foreach (char a in result) Console.Write (a);
+      List<int> ints = new ();
+      for (int i = 0, count = 0; i < result.Length; i++) {
+         for (int j = 0; j < result.Length; j++) if (result[i] == result[j]) count++;
+         ints.Add (count);
+         count = 0;
+      }
+      if (ints.Sum () == result.Length) {
+         if (result == "") Console.WriteLine ("\"\"");
+         else foreach (char a in result) Console.Write (a);
+         return;
+      }
+      string crossCheck = AvoidAdjPairs (result);
+      if (crossCheck == "") Console.WriteLine ("\"\"");
+      else foreach (char a in crossCheck) Console.Write (a);
    }
 
    static string AvoidAdjPairs (string input) {
+
       List<char> inputCharsList = new (), tempInput = input.ToList ();
       int lastRepIndex = 0, j;
+      int DuplicateStringReducer (int count) {
+         while (count != 0) {
+            tempInput.RemoveAt (lastRepIndex);
+            lastRepIndex--;
+            count--;
+         }
+         return count;
+      }
       for (int i = 0; i < tempInput.Count; i++) {
          int count = 0; char item = ' ';
          for (j = 0; j < tempInput.Count; j++) {
@@ -28,38 +49,20 @@ internal class Program {
                count--;
                j = -1;
             } else if (count % 2 == 0) {
-               while (count != 0) {
-                  tempInput.RemoveAt (lastRepIndex);
-                  lastRepIndex--;
-                  count--;
-               }
+               count = DuplicateStringReducer (count);
                j = -1;
             } else {
-               while (count != 0) {
-                  tempInput.RemoveAt (lastRepIndex);
-                  lastRepIndex--;
-                  count--;
-               }
+               count = DuplicateStringReducer (count);
                inputCharsList.Add (item);
                j = -1;
             }
          }
          if (count % 2 != 0) {
-            while (count != 0) {
-               tempInput.RemoveAt (lastRepIndex);
-               lastRepIndex--;
-               count--;
-            }
+            count = DuplicateStringReducer (count);
             inputCharsList.Add (item);
             j = -1;
          } else if (count == 0 || count == 1) inputCharsList.Add (tempInput[i]);
-         else if (count % 2 == 0) {
-            while (count != 0) {
-               tempInput.RemoveAt (lastRepIndex);
-               lastRepIndex--;
-               count--;
-            }
-         }
+         else if (count % 2 == 0) count = DuplicateStringReducer (count);
       }
       if (inputCharsList.Count == 2) if (inputCharsList[0] == inputCharsList[1]) inputCharsList.Clear ();
       return new string (inputCharsList.ToArray ());
