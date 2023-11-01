5077eb3395436ed42c238058e63e61ed4e9b3ea5
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-11-01 11:42:56 +0530

    T01 implemented

--- T01/Program.cs
+++ T01/Program.cs
@@ -1,15 +1,21 @@
-﻿//Convert THE  GIVEN DECIMAL NUMBER INTO BASE VALUE OF 16,8 AND 2
+﻿// Convert THE GIVEN DECIMAL NUMBER INTO BASE VALUE OF 16,8 AND 2
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Enter the decimal number ");
-      int user_no = Convert.ToInt32 (Console.ReadLine ());
-      Console.WriteLine ("\nwhich base value you want to convert\n\tif you want to convert" +
-         " Hexadecimal number then press 'h'\n\tif you want to convert Octal then press 'o'" +
-         "\n\tif you want to convert Binary numbers then press 'b'\n");
-      var user_respond_key = Console.ReadKey (true).KeyChar;
-      if (user_respond_key == 'h') Console.WriteLine (Convert.ToString (user_no, 16));
-      else if (user_respond_key == 'o') Console.WriteLine (Convert.ToString (user_no, 8));
-      else if (user_respond_key == 'b') Console.WriteLine (Convert.ToString (user_no, 2));
-      else Console.WriteLine ("Your press the wrong button");
+      Console.Write ("Enter the decimal based numbers ---> ");
+      if (!int.TryParse (Console.ReadLine (), out var userNum)) {
+         Console.WriteLine ("Invaild input");
+         return;
+      }
+      Console.WriteLine ("which base value you want to convert\n\t1. If you want to convert" +
+         " Hexadecimal number then press 'h'.\n\t2. If you want to convert Octal then press 'o'." +
+         "\n\t3. If you want to convert Binary numbers then press 'b'.\n");
+      var userRespondKey = Console.ReadKey (true).KeyChar;
+      if (userRespondKey == 'h')
+         Console.WriteLine ("Hexadecimal number is " + Convert.ToString (userNum, 16));
+      else if (userRespondKey == 'o')
+         Console.WriteLine ("Octal number is " + Convert.ToString (userNum, 8));
+      else if (userRespondKey == 'b')
+         Console.WriteLine ("Binary Number is " + Convert.ToString (userNum, 2));
+      else Console.WriteLine ("Your press the wrong button.");
    }
 }
