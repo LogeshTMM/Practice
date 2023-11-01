96a97e860d04d697560f1e4f6a4e49f444ce741e
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-11-01 14:12:51 +0530

    T01 implemented

--- T01/Program.cs
+++ T01/Program.cs
@@ -9,13 +9,19 @@ internal class Program {
       Console.WriteLine ("which base value you want to convert\n\t1. If you want to convert" +
          " Hexadecimal number then press 'h'.\n\t2. If you want to convert Octal then press 'o'." +
          "\n\t3. If you want to convert Binary numbers then press 'b'.\n");
-      var userRespondKey = Console.ReadKey (true).KeyChar;
-      if (userRespondKey == 'h')
-         Console.WriteLine ("Hexadecimal number is " + Convert.ToString (userNum, 16));
-      else if (userRespondKey == 'o')
-         Console.WriteLine ("Octal number is " + Convert.ToString (userNum, 8));
-      else if (userRespondKey == 'b')
-         Console.WriteLine ("Binary Number is " + Convert.ToString (userNum, 2));
-      else Console.WriteLine ("Your press the wrong button.");
+      switch (Console.ReadKey (true).KeyChar) {
+         case 'h':
+            Console.WriteLine ("Hexadecimal number is " + Convert.ToString (userNum, 16));
+            break;
+         case 'o':
+            Console.WriteLine ("Octal number is " + Convert.ToString (userNum, 8));
+            break;
+         case 'b':
+            Console.WriteLine ("Binary Number is " + Convert.ToString (userNum, 2));
+            break;
+         default:
+            Console.WriteLine ("You pressed the wrong button.");
+            break;
+      }
    }
 }
