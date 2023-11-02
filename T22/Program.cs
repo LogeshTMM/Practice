c1156d0bae5e06ec94b3227ee6f3859deb708801
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-11-02 13:24:42 +0530

    T22 implemented

--- T22/Program.cs
+++ T22/Program.cs
@@ -1,21 +1,19 @@
-﻿// Training ~ A training program for new joinees at Metamation, Batch- July 2023
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2023
 // Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------
 // Program.cs
 // <TODO: Display random series of numbers to the user and ask them to provide two
-// index values which need to be swapped.
-// The output displays a swapped random number series to the user.>
-
+// index values which need to be swapped.>
+// ---------------------------------------------------------------------------------------
 internal class Program {
    static void Main () {
-      Console.WriteLine ("Random number series given below");
-      int[] ints = Array.Empty<int> ();
-      for (int i = 0; i < 10; i++) {
-         Random rnd = new ();
-         int number = rnd.Next (i, 10);
-         ints = ints.Append (number).ToArray ();
-      }
+      Console.WriteLine ("Random number series given below:");
+      int[] ints = new int[10];
+      Random rnd = new ();
+      for (int i = 0; i < 10; i++) ints[i] = rnd.Next (1, 10);
       Console.Write (string.Join (" ", ints));
-      Console.WriteLine ("\nJust type the two index numbers to swap");
+      Console.WriteLine ("\nJust type the two index numbers to swap the values in the series.");
       bool firstValue = int.TryParse (Console.ReadLine (), out int num1);
       bool secondValue = int.TryParse (Console.ReadLine (), out int num2);
       if (!firstValue | !secondValue | num1 == num2) Console.WriteLine ("Invaild input");
@@ -23,18 +21,18 @@ internal class Program {
          Console.Write ("After swapping the values in the number series: " + string.Join (" ", ints));
    }
 
-   /// <summary>Swapping the two values in the series.</summary>
+   /// <summary>Swapping the two values in the number series.</summary>
    /// <param name="a">Getting the first index value from the user.</param>
    /// <param name="b">Getting the second index value from the user.</param>
-   /// <param name="ints">Random number series.</param>
+   /// <param name="numberSeries">Random number series.</param>
    /// <returns> Return values:
-   /// (Default): Swapped the two values in the series.
+   /// True: Swapped the two values in the number series.
    /// </returns>
-   static bool SwapNumbers (int a, int b, ref int[] ints) {
-      if (a >= ints.Length | b >= ints.Length) {
+   static bool SwapNumbers (int a, int b, ref int[] numberSeries) {
+      if (a >= numberSeries.Length | b >= numberSeries.Length) {
          Console.WriteLine ("Index out of range");
          return false;
-      } else (ints[a], ints[b]) = (ints[b], ints[a]);
+      } else (numberSeries[a], numberSeries[b]) = (numberSeries[b], numberSeries[a]);
       return true;
    }
 }
