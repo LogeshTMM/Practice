4f779241b82504be38b102faaf0685e38dcb036e
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-11-07 10:34:35 +0530

    T07 implemented

--- T07/Program.cs
+++ T07/Program.cs
@@ -7,29 +7,46 @@ using System.Text;
 internal class Program {
    static void Main (string[] args) {
       Console.OutputEncoding = new UnicodeEncoding ();
-      for (int i = 0; i < 8; i++) Console.Write ("\u250f\u2501\u2501\u2501");
-      Console.WriteLine ("\u2513");
+      Console.Write ("\u250f");
+      FirstTwoBlockLines ("\u2533");
+      Console.WriteLine ("\u2513"); // 7 L shape unicode
       Console.WriteLine ("\u2503 \u265c ┃ \u265E ┃ \u265D ┃ \u265B ┃ \u265A ┃ \u265D ┃ \u265E ┃ \u265C ┃");
-      for (int i = 0; i < 8; i++) Console.Write ("\u2523\u2501\u2501\u2501");
-      Console.WriteLine ("\u252B");
+      Console.Write ("\u2523");
+      FirstTwoBlockLines ("\u254B"); // plus heavy unicode
+      Console.WriteLine ("\u252B"); // T shape unicode
       for (int i = 0; i < 8; i++) Console.Write ("\u2503 \u265F ");
       Console.WriteLine ("┃");
-      for (int i = 0; i < 8; i++) Console.Write ("┣\u2501\u2501\u2501");
+      Console.Write ("┣");
+      FirstTwoBlockLines ("\u254B");
       Console.WriteLine ("\u252B");
       int j = 0;
       while (j != 4) {
          for (int i = 0; i < 9; i++) Console.Write ("┃   ");
          Console.WriteLine ();
-         for (int i = 0; i < 8; i++) Console.Write ("┣\u2501\u2501\u2501");
+         RemainingBlockLines ("┣", "\u254B");
          Console.WriteLine ("\u252B");
          j++;
       }
       for (int i = 0; i < 8; i++) Console.Write ("\u2503 \u2659 ");
       Console.WriteLine ("\u2503");
-      for (int i = 0; i < 8; i++) Console.Write ("┣\u2501\u2501\u2501");
+      RemainingBlockLines ("┣", "\u254B");
       Console.WriteLine ("┫");
       Console.WriteLine ("\u2503 \u2656 ┃ \u2658 ┃ \u2657 ┃ \u2655 ┃ \u2654 ┃ \u2657 ┃ \u2658 ┃ \u2656 ┃");
-      for (int i = 0; i < 8; i++) Console.Write ("\u2517\u2501\u2501\u2501");
+      RemainingBlockLines ("\u2517", "\u253B");
       Console.WriteLine ("\u251B");
+
+      void FirstTwoBlockLines (string line) {
+         for (int i = 0; i < 8; i++) {
+            Console.Write ("\u2501\u2501\u2501");
+            if (i != 7) Console.Write ($"{line}");
+         }
+      }
+
+      void RemainingBlockLines (string a, string b) {
+         for (int i = 0; i < 8; i++) {
+            if (i == 0) Console.Write ($"{a}\u2501\u2501\u2501");
+            else Console.Write ($"{b}\u2501\u2501\u2501");
+         }
+      }
    }
 }
