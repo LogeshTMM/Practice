d218fd18232062876260a6799d3a584a5d08dffb
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-11-14 17:22:34 +0530

    T08 implemented

--- Training/Program.cs
+++ Training/Program.cs
@@ -1,9 +1,20 @@
-﻿using System;
-
-Console.WriteLine ("Hello, World!");
-for (int i = 0; i < 10; i++)
-   Console.Write ($"{i}   ");
-Console.Write ("\nEnter name: ");
-var line = Console.ReadLine ();
-Console.WriteLine ("{0} {1}!!", "Hello", line);
-Console.WriteLine ();
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2023
+// Copyright (c) Metamation India.
+// ---------------------------------------------------------------------------
+// Program.cs
+// <TODO: To form a multiplication table.
+// Display the first 10 values of a multiplicatin table based on the user input.>
+// ---------------------------------------------------------------------------------------
+using System;
+internal class Program {
+   static void Main (string[] args) {
+      Console.Write ("Enter the number to get the multiplication table upto 10: ");
+      _ = int.TryParse (Console.ReadLine (), out int num);
+      if (num < 1) {
+         Console.WriteLine ("Invalid input");
+         return;
+      }
+      for (int i = 1; i < 11; i++) Console.WriteLine ($"{num} * {i} = {num * i}");
+   }
+}
