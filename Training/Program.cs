00ccabf5714f66e0f9b23266d9ea5a7529dbccc2
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-11-27 10:06:54 +0530

    A03 implemented

--- Training/Program.cs
+++ Training/Program.cs
@@ -13,47 +13,32 @@ using System.Linq;
 internal class Program {
    static void Main (string[] args) {
       string[] words = File.ReadAllLines ("D:/LogeshKumar.P/words.txt"); // File location should be as per your system and focus on backslash, otherwise an error might be shown.
-      Console.Write ("Enter the seven letters: ");
-      string sevenLetters = Console.ReadLine ().ToUpper ();// Note: The first letter in the string must be used to create a word.
+      Console.Write ("Enter the seven letters: ");// Note: The first letter in the string must be used to create a word.
+      string sevenLetters = Console.ReadLine ().ToUpper ();//ToUpper () method is used because the document has capital letters.
       if (!sevenLetters.All (char.IsLetter) || sevenLetters == "" || sevenLetters.Length > 7 || sevenLetters.Length < 7) {
-         Console.WriteLine ("Invaild input");
+         Console.WriteLine ("Invalid input");
          return;
       }
       Dictionary<string, int> spellBee = new ();
       foreach (var word in words) {
          if (word.Length >= 4 && word.Contains (sevenLetters[0])) {
             int count = 0;
-            for (int i = 0; i < sevenLetters.Length; i++) if (word.Contains (sevenLetters[i])) count++; // It doesn't check for duplicate characters in the string.
-            int sumOfDuplicatechars = 0; // Total number of duplicate characters in the string, except for the original characters.
-            List<char> chars = word.ToList ();
-            for (int i = 0; i < chars.Count; i++) { // This for loop is used to count duplicate characters, except for the original characters.
-               char temp = chars[i];
-               int tempCounter = 0;
-               while (chars.Contains (temp)) {
-                  chars.Remove (temp);
-                  tempCounter++;
-               }
-               if (tempCounter % 2 == 0) sumOfDuplicatechars += tempCounter / 2;
-               else if (tempCounter % 2 != 0) sumOfDuplicatechars += tempCounter - 1;
-               i = -1; // To start the loop from 0, because of randomly removing characters from the string.
-            }
-            if ((sumOfDuplicatechars + count) == word.Length) {
+            List<char> chars = word.Distinct ().ToList ();
+            for (int i = 0; i < sevenLetters.Length; i++) if (chars.Contains (sevenLetters[i])) count++;
+            if (count == chars.Count) {
                if (count == sevenLetters.Length) spellBee.Add (word, word.Length + 7); // (word.Length + 7) => Pangram, addition of 7 is a bonus point.
                else if (word.Length == 4) spellBee.Add (word, 1);
                else spellBee.Add (word, word.Length);
             }
          }
       }
-      var tempDic = spellBee.OrderByDescending (x => x.Value);
       int totalValue = 0;
-      foreach (var kvp in tempDic) {
+      foreach (var kvp in spellBee.OrderByDescending (x => x.Value)) {
          if (kvp.Value > kvp.Key.Length) {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine ($"{kvp.Value,2}.  {kvp.Key}", Console.ForegroundColor);
-         } else {
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine ($"{kvp.Value,2}.  {kvp.Key}", Console.ForegroundColor);
-         }
+            Console.WriteLine ($"{kvp.Value,2}. {kvp.Key}", Console.ForegroundColor);
+            Console.ResetColor ();
+         } else Console.WriteLine ($"{kvp.Value,2}. {kvp.Key}");
          totalValue += kvp.Value;
       }
       Console.WriteLine ("----\n" + totalValue + " " + "total");
