d5457de95f3ebc9b6e5b78d6ada705e915502a66
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-12-14 11:44:32 +0530

    correction implemented

--- Training/Program.cs
+++ Training/Program.cs
@@ -12,25 +12,25 @@ using System.IO;
 using System.Linq;
 internal class Program {
    static void Main (string[] args) {
-      string[] words = File.ReadAllLines ("D:/LogeshKumar.P/words.txt"); // File location should be as per your system and focus on forward slash, otherwise an error might be shown.
+      string[] words = File.ReadAllLines ("C:/etc/words.txt"); // File location should be as per your system and focus on forward slash, otherwise an error might be shown.
       Console.Write ("Enter the seven letters: ");// Note: The first letter in the string must be used to create a word.
       string sevenLetters = Console.ReadLine ().ToUpper ();//ToUpper () method is used because the document has capital letters.
-      if (!sevenLetters.All (char.IsLetter) || sevenLetters == "" || sevenLetters.Length > 7 || sevenLetters.Length < 7) {
+      if (!sevenLetters.All (char.IsLetter) || sevenLetters.Length > 7 || sevenLetters.Length != 7) {
          Console.WriteLine ("Invalid input");
          return;
       }
       Dictionary<string, int> spellBee = new ();
       foreach (var word in words) {
          List<char> chars = word.Distinct ().ToList ();
-         if (word.Length >= 4 && word.Contains (sevenLetters[0]) && chars.All (a => sevenLetters.ToArray ().Contains (a)))
-            spellBee.Add (word, (chars.Count == sevenLetters.Length) ? word.Length + 7 : (word.Length > 4) ? word.Length : 1);
+         if (word.Length >= 4 && word.Contains (sevenLetters[0]) && chars.All (a => sevenLetters.Contains (a)))
+            spellBee.Add (word, (chars.Count == 7) ? word.Length + 7 : (word.Length > 4) ? word.Length : 1);
          // The addition of 7 is a bonus point as Pangram is denoted.
       }
       int totalValue = 0; // The sum of points earned from the spell bee words.
       foreach (var kvp in spellBee.OrderByDescending (x => x.Value)) {
          if (kvp.Value > kvp.Key.Length) {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine ($"{kvp.Value,3}. {kvp.Key}", Console.ForegroundColor);
+            Console.WriteLine ($"{kvp.Value,3}. {kvp.Key}");
             Console.ResetColor ();
          } else Console.WriteLine ($"{kvp.Value,3}. {kvp.Key}");// Modify the padding value based on the digits of the total value.
          totalValue += kvp.Value;
