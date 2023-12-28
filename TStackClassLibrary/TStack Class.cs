e596a39b6b01630f039d234e346970ffd04cd020
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-12-28 12:01:08 +0530

    T27 implemented

---
+++ TStackClassLibrary/TStack Class.cs
@@ -0,0 +1,66 @@
+﻿namespace TStack;
+public class TStack<T> {
+
+   public TStack () => mArray = Array.Empty<T> ();
+
+   public TStack (int capacity) {
+      if (capacity < 0) throw new Exception ("Cannot create a TStack with a negative size");
+      else mArray = Array.Empty<T> ();
+   }
+
+   public int Capacity {
+      get {
+         if (mArray.Length > mCapacity) {
+            int quotient = mArray.Length / 4, counter = 0;// Based on the quotient value capacity of TStack will be determined.
+            while (counter != quotient) {
+               mCapacity *= 2;
+               counter++;
+            }
+            return mCapacity;
+         }
+         return mCapacity;
+      }
+   }
+
+   public void Push (T element) {
+      if (mSize == 0) {
+         T[] temparr = mArray;
+         int mArrayNewLen = mArray.Length + 1, mArrayOldLen = temparr.Length - 1;
+         mArray = new T[mArrayNewLen];
+         while (mArrayOldLen != -1)
+            mArray[--mArrayNewLen] = temparr[mArrayOldLen--];
+         mSize = mArrayNewLen;
+      }
+      mArray[--mSize] = element;
+   }
+
+   public T Peek () {
+      if (mArray.Length == 0) throw new Exception ("InvalidOperationException");
+      else return mArray[0];
+   }
+
+   public T Pop () {
+      T[] tempArr = mArray;
+      if (mArray.Length == 0) throw new Exception ("InvalidOperationException");
+      mArray = new T[tempArr.Length - 1];
+      int mArrayNewLen = tempArr.Length - 1, mArrayOldlen = tempArr.Length - 1;
+      while (mArrayOldlen != 0)
+         mArray[--mArrayNewLen] = tempArr[mArrayOldlen--];
+      return tempArr[0];
+   }
+
+   public int Count {
+      get => mSize = mArray.Length;
+   }
+
+   public bool IsEmpty {
+      get {
+         if (mArray.Length == 0) return true;
+         return false;
+      }
+   }
+
+   T[] mArray;
+   int mSize;
+   int mCapacity = 4;
+}
---
+++ TStackTestProject/TStack UnitTest.cs
@@ -0,0 +1,67 @@
+using TStack;
+namespace TestProject_T27 {
+   [TestClass]
+   public class UnitTest1 {
+      static readonly TStack<int> tstack = new (); // User-Defined class
+      static readonly Stack<int> stacks = new (); // Predefined class
+
+      // In the following four methods, mylist and lists are not initialized, except for the first and second TestMethod.
+
+      [TestMethod]
+      ///<summary>In this method we add the elements to the TStack<T> (user - defined class) methods like Push and Pop
+      ///as well as Stack<T> (predefined class). Lastly, evaluate the elements in both classes. </summary>
+      public void TestMethod1 () {
+         int i = 0;
+         while (i != 5) {
+            tstack.Push (i);
+            stacks.Push (i++);
+         }
+         while (i != 0) {
+            Assert.AreEqual (stacks.Pop (), tstack.Pop ());
+            i--;
+         }
+      }
+
+      [TestMethod]
+      ///<summary>In this method we add the elements to the TStack<T> (user - defined class) methods like Push and Peekalong
+      ///with property IsEmpty as well as Stack<T> (predefined class) except IsEmpty. Lastly, evaluate the elements in both
+      ///classes also check the tstack is empty or not. </summary>
+      public void TestMethod2 () {
+         int i = 0;
+         while (i != 6) {
+            tstack.Push (i);
+            stacks.Push (i++);
+         }
+         Assert.IsTrue (tstack.Peek () == stacks.Peek ());
+         Assert.IsFalse (tstack.IsEmpty);
+      }
+
+      [TestMethod]
+      ///<summary>In this method we evaluate the number of elements in the TStack<T> (user - defined class)
+      /// as well as Stack<T> (predefined class). Lastly, evaluate the total number of elements in both classes. </summary>
+      public void TestMethod3 () => Assert.IsTrue (tstack.Count == stacks.Count);
+
+      [TestMethod]
+      ///<summary>In this method we delete the elements in the TStack<T> (user - defined class)
+      ///as well as Stack<T> (predefined class). Lastly, evaluate the total number of elements in both classes 
+      ///also check the tstack is empty or not. </summary>
+      public void TestMethod4 () {
+         while (tstack.Count != 0) {
+            tstack.Pop ();
+            stacks.Pop ();
+         }
+         Assert.IsTrue (tstack.IsEmpty);
+         Assert.AreEqual (tstack.Count, stacks.Count);
+      }
+
+      [TestMethod]
+      ///<summary>In this method we check an exception in the TStack<T> (user - defined class) while using Peek method
+      ///in an empty tstack. Lastly, evaluate whether is throws an exception or not. </summary>
+      public void TestMethod5 () => Assert.ThrowsException<Exception> (() => tstack.Peek ());
+
+      [TestMethod]
+      ///<summary>In this method we check an exception in the TStack<T> (user - defined class) while using Pop method
+      ///in an empty tstack. Lastly, evaluate whether is throws an exception or not. </summary>
+      public void TestMethod6 () => Assert.ThrowsException<Exception> (() => tstack.Pop ());
+   }
+}
