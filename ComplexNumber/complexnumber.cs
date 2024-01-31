1ca89fca3508894cf34d8720a39f2fd659e4151f
LogeshTMM <logeshkumar.p.interns@metamation.com>
2024-01-31 16:26:15 +0530

    Corrected

--- ComplexNumber/complexnumber.cs
+++ ComplexNumber/complexnumber.cs
@@ -4,14 +4,14 @@ public class ComplexNumber {
    // Second complex number syntax based on method parameters: bReal + bImg
 
    // The user provides four parameter values for the addition operation between two complex numbers.
-   public string Addition (double aReal, double aImg, double bReal, double bImg)
+   public static string Addition (double aReal, double aImg, double bReal, double bImg)
       => $"{aReal + bReal} + {aImg + bImg}i";
 
    // The user provides four parameter values for the subtraction operation between two complex numbers.
-   public string Subraction (double aReal, double aImg, double bReal, double bImg)
+   public static string Subraction (double aReal, double aImg, double bReal, double bImg)
       => $"{aReal - bReal} - ({aImg - bImg}i)";
 
    // It performs a magnitude operation on a single complex number.
-   public double Magnitude (double aReal, double aimg)
-      => Math.Sqrt (Math.Pow (aReal, 2) + Math.Pow (aimg, 2));
+   public static double Magnitude (double aReal, double aImg)
+      => Math.Sqrt (Math.Pow (aReal, 2) + Math.Pow (aImg, 2));
 }
--- Training/Program.cs
+++ Training/Program.cs
@@ -4,7 +4,8 @@ using System;
 internal class Program {
    static void Main (string[] args) {
       double aReal, aImg, bReal, bImg;
-      ComplexNumber number = new ();
+      Console.WriteLine ("Note: If you fail to enter the numbers, it will repeat" +
+         " until you enter the number values.\n");
       Console.Write ("Enter the first complex number followed by real number: ");
       for (; ; ) if (double.TryParse (Console.ReadLine (), out aReal)) break;
       Console.Write ("Enter the first complex number followed by imaginery number: ");
@@ -13,13 +14,13 @@ internal class Program {
       for (; ; ) if (double.TryParse (Console.ReadLine (), out bReal)) break; ;
       Console.Write ("Enter the second complex number followed by imaginery number: ");
       for (; ; ) if (double.TryParse (Console.ReadLine (), out bImg)) break; ;
-      Console.WriteLine ("\nResult of addition operation between two complex numbers is :"
-         + number.Addition (aReal, aImg, bReal, bImg));
-      Console.WriteLine ("\nResult of subtraction operation between two complex numbers is :"
-         + number.Subraction (aReal, aImg, bReal, bImg));
-      Console.WriteLine ("\nResult of magnitude operation between first complex numbers is :"
-         + number.Magnitude (aReal, aImg));
-      Console.WriteLine ("\nResult of magnitude operation between second complex numbers is :"
-         + number.Magnitude (aReal, aImg));
+      Console.WriteLine ("\nResult of addition operation between two complex numbers is : "
+         + ComplexNumber.Addition (aReal, aImg, bReal, bImg));
+      Console.WriteLine ("\nResult of subtraction operation between two complex numbers is : "
+         + ComplexNumber.Subraction (aReal, aImg, bReal, bImg));
+      Console.WriteLine ("\nResult of magnitude operation between first complex numbers is : "
+         + ComplexNumber.Magnitude (aReal, aImg));
+      Console.WriteLine ("\nResult of magnitude operation between second complex numbers is : "
+         + ComplexNumber.Magnitude (aReal, aImg));
    }
 }
